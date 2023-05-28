29e14df4ee27c59e2adf7db9bfb0a322e5f84565
OIegZolotarev <ovzolotarev@gmail.com>
2023-05-28 10:34:10 +0300

    Команда добавления структуры модуля.

--- bundles/org.example.ui/src/org/quiteoldrange/i3textutils/ModuleRefactoringUtils.java
+++ bundles/org.example.ui/src/org/quiteoldrange/i3textutils/ModuleRefactoringUtils.java
@@ -9,11 +9,14 @@ import java.util.List;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.ui.IFileEditorInput;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.handlers.HandlerUtil;
+import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.ui.editor.XtextEditor;
 import org.eclipse.xtext.ui.editor.model.IXtextDocument;
+import org.eclipse.xtext.util.concurrent.IUnitOfWork;
 
 import com._1c.g5.v8.dt.bsl.model.Method;
 import com._1c.g5.v8.dt.bsl.model.Module;
@@ -73,7 +76,7 @@ public class ModuleRefactoringUtils
      * @param event - событие полученное плагином
      * @return документ с которым может работать документ
      */
-    public static IXtextDocument GetXTextDocumentFromEvent(ExecutionEvent event)
+    public static IXtextDocument getXTextDocumentFromEvent(ExecutionEvent event)
     {
         //
         IWorkbenchPart part = HandlerUtil.getActivePart(event);
@@ -100,6 +103,59 @@ public class ModuleRefactoringUtils
 
         return null;
 
+    }
+
+    /**
+     * Получает проект из редактора
+     * @param event - событие полученное плагином
+     * @return проект
+     */
+    public static IProject getProjectFromEvent(ExecutionEvent event)
+    {
+        //
+        IWorkbenchPart part = HandlerUtil.getActivePart(event);
+        XtextEditor target = part.getAdapter(XtextEditor.class);
+
+        if (target != null)
+        {
+            if (!(target.getEditorInput() instanceof IFileEditorInput))
+                return null;
+
+            IFileEditorInput input = (IFileEditorInput)target.getEditorInput();
+            IFile file = input.getFile();
+
+            if (file == null)
+                return null;
+
+            IProject project = file.getProject();
+
+            return project;
+        }
+
+        return null;
+
+    }
+
+    public static Module getModuleModel(IXtextDocument doc)
+    {
+        return (Module)doc.readOnly(new IUnitOfWork<EObject, XtextResource>()
+        {
+            @Override
+            public EObject exec(XtextResource res) throws Exception
+            {
+                //сперва проверяем, доступность семантической модели встроенного языка
+                if (res.getContents() != null && !res.getContents().isEmpty())
+                {
+                    EObject obj = res.getContents().get(0);
+                    if (obj instanceof Module) // проверили, что работаем с правильным объектом семантической модели
+                    {
+                        Module module = (Module)obj;
+                        return module;
+                    }
+                }
+                return null;
+            }
+        });
     }
 
 }
---
+++ bundles/org.example.ui/src/org/quiteoldrange/i3textutils/ModuleStructureFiller.java
@@ -0,0 +1,108 @@
+/**
+ *
+ */
+package org.quiteoldrange.i3textutils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.xtext.ui.editor.model.IXtextDocument;
+
+import com._1c.g5.v8.dt.bsl.model.Module;
+import com._1c.g5.v8.dt.bsl.model.ModuleType;
+
+/**
+ * @author ozolotarev
+ *
+ */
+public class ModuleStructureFiller
+    extends AbstractHandler
+{
+
+    public void fillModuleStructure(IXtextDocument doc, IProject project) throws IOException, BadLocationException
+
+    {
+        Module moduleModel = ModuleRefactoringUtils.getModuleModel(doc);
+
+        if (moduleModel == null)
+            return;
+
+        IFile templatePath = project.getFile(getFileTemplatePathForModuleType(moduleModel.getModuleType()));
+        File f = templatePath.getLocation().toFile();
+
+        String template = new String(Files.readAllBytes(Paths.get(f.getAbsolutePath())));
+
+        doc.replace(0, 0, template);
+
+    }
+
+    private String getFileTemplatePathForModuleType(ModuleType type)
+    {
+        if (type == ModuleType.COMMON_MODULE)
+            return ".settings/templates/common_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.ORDINARY_APP_MODULE)
+            return ".settings/templates/ordinary_app_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.BOT_MODULE)
+            return ".settings/templates/bot_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.COMMAND_MODULE)
+            return ".settings/templates/command_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.EXTERNAL_CONN_MODULE)
+            return ".settings/templates/external_conn_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.FORM_MODULE)
+            return ".settings/templates/form_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.HTTP_SERVICE_MODULE)
+            return ".settings/templates/http_service_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.INTEGRATION_SERVICE_MODULE)
+            return ".settings/templates/integration_service_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.MANAGED_APP_MODULE)
+            return ".settings/templates/managed_app_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.MANAGER_MODULE)
+            return ".settings/templates/manager_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.OBJECT_MODULE)
+            return ".settings/templates/object_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.RECORDSET_MODULE)
+            return ".settings/templates/recordset_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.SESSION_MODULE)
+            return ".settings/templates/session_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.VALUE_MANAGER_MODULE)
+            return ".settings/templates/value_manager_module.bsl"; //$NON-NLS-1$
+        else if (type == ModuleType.WEB_SERVICE_MODULE)
+            return ".settings/templates/web_service_module.bsl"; //$NON-NLS-1$
+
+        return null;
+    }
+
+    @Override
+    public Object execute(ExecutionEvent event) throws ExecutionException
+    {
+        IProject project = ModuleRefactoringUtils.getProjectFromEvent(event);
+
+        if (project == null)
+            return null;
+
+        IXtextDocument xTextDocument = ModuleRefactoringUtils.getXTextDocumentFromEvent(event);
+
+        if (xTextDocument == null)
+            return null;
+
+        try
+        {
+            fillModuleStructure(xTextDocument, project);
+        }
+        catch (IOException | BadLocationException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+}

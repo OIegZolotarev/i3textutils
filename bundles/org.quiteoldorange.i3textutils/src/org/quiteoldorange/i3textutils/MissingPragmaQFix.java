1fdf8e1876a4aa529e5648f3d01f040e909d9b04
Олег Золотарев <ovzolotarev@gmail.com>
2023-06-19 13:23:31 +0300

    Настройка под офисный комп

--- bundles/org.quiteoldorange.i3textutils/src/org/quiteoldorange/i3textutils/MissingPragmaQFix.java
+++ bundles/org.quiteoldorange.i3textutils/src/org/quiteoldorange/i3textutils/MissingPragmaQFix.java
@@ -11,13 +11,15 @@ import com.e1c.g5.v8.dt.bsl.check.qfix.IXtextBslModuleFixModel;
 import com.e1c.g5.v8.dt.bsl.check.qfix.SingleVariantXtextBslModuleFix;
 import com.e1c.g5.v8.dt.check.qfix.components.QuickFix;
 
+
+
 /**
  * @author ozolotarev
  *
  */
 
 // checkId + supplierId идентифицируют ошибку!
-@QuickFix(checkId = "form-module-missing-pragma", supplierId = "com.e1c.v8codestyle.bsl")
+@QuickFix(checkId = "form-module-missing-pragma11", supplierId = "com.e1c.v8codestyle.bsl")
 public class MissingPragmaQFix
     extends SingleVariantXtextBslModuleFix
 {

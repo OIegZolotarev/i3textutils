c61832ab896661221a05d65ca9a24e4b0a0557c0
Олег Золотарев <ovzolotarev@gmail.com>
2023-06-23 10:19:35 +0300

    Update Lexer.java

--- bundles/org.quiteoldorange.i3textutils/src/org/quiteoldorange/i3textutils/bsl/parser/Lexer.java
+++ bundles/org.quiteoldorange.i3textutils/src/org/quiteoldorange/i3textutils/bsl/parser/Lexer.java
@@ -3,15 +3,50 @@
  */
 package org.quiteoldorange.i3textutils.bsl.parser;
 
+import java.util.Stack;
+
 /**
  * @author ozolotarev
  *
  */
 public class Lexer
 {
+    private int mOffset;
+    private Stack<Token> mTokensStack;
 
     public Lexer(String source)
     {
+        mOffset = 0;
+        mTokensStack = new Stack<>();
+    }
 
+    Token parseNext()
+    {
+        Token result = new Token(null,null, 0,0,0);
+
+        mTokensStack.push(result);
+        return result;
+    }
+
+    Token top()
+    {
+        return mTokensStack.peek();
+    }
+
+    Token pop()
+    {
+        Token r = mTokensStack.pop();
+        mOffset = r.getOffset();
+        return r;
+    }
+
+    public int getOffset()
+    {
+        return mOffset;
+    }
+
+    public void setOffset(int offset)
+    {
+        mOffset = offset;
     }
 }

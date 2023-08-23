47191f884745d84d532b26e1520f35b450ccea17
Олег Золотарев <ovzolotarev@gmail.com>
2023-08-23 09:47:28 +0300

    Исправление бага в preferences

--- bundles/org.quiteoldorange.i3textutils/src/org/quiteoldorange/i3textutils/preferences/PreferenceConstants.java
+++ bundles/org.quiteoldorange.i3textutils/src/org/quiteoldorange/i3textutils/preferences/PreferenceConstants.java
@@ -8,8 +8,9 @@ public class PreferenceConstants {
     public static final String FORCE_CONTENT_ASSIST_COLOR_HACK = "forceContentAssistColorHack"; //$NON-NLS-1$
     public static final String FORCE_CONTENT_ASSIST_COLOR_HACK_VALUE = "forceContentAssistColorHackValue"; //$NON-NLS-1$
 
-    public static final String CODEMININGS_ENABLED = "forceContentAssistColorHackValue"; //$NON-NLS-1$
-    public static final String CODEMININGS_SHOW_WHEN_ONE_PARAMETER = "forceContentAssistColorHackValue"; //$NON-NLS-1$
-    public static final String CODEMININGS_SHOW_WHEN_INPUT_CONTAINS_PARAMETER_NAME = "forceContentAssistColorHackValue"; //$NON-NLS-1$
+    public static final String CODEMININGS_ENABLED = "codeMinigsEnabled"; //$NON-NLS-1$
+    public static final String CODEMININGS_SHOW_WHEN_ONE_PARAMETER = "codeminigsShowWhenOneParameter"; //$NON-NLS-1$
+    public static final String CODEMININGS_SHOW_WHEN_INPUT_CONTAINS_PARAMETER_NAME =
+        "codeminingsShowWhenInputContainsParameterName"; //$NON-NLS-1$
 
 }
